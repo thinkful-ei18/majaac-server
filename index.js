b743abf5f13482f062a3d0ca0a75ee879729e9bb
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-05-01 13:39:12 -0700

    passport.use & auth/user router in index.js

--- index.js
+++ index.js
@@ -3,17 +3,25 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const passport = require('passport');
 const bodyParser = require('body-parser');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 
 const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+const { router: usersRouter } = require('./routes/users.js');
 const markerRouter = require('./Marker/markerRouter');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/users', usersRouter);
+app.use('/api/auth', authRouter);
 
 app.use('/api', markerRouter);
 
