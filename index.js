0241c0e4a6835a25449e764b04fc5156571c6db7
Christina Moore <sayhitochristina@gmail.com>
2018-05-02 10:49:34 -0500

    CORS reorder

--- index.js
+++ index.js
@@ -8,17 +8,25 @@ const bodyParser = require('body-parser');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
-
-const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
-const { router: usersRouter } = require('./routes/users.js');
 const markerRouter = require('./Marker/markerRouter');
 
 const app = express();
+
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+const { router: usersRouter } = require('./routes/users.js');
+
 
 app.use(bodyParser.json());
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
+
+// ORDER VERY IMPORTANT, DO NOT MOVE
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN,
+    })
+);
 
 app.use('/api/users', usersRouter);
 app.use('/api/auth', authRouter);
@@ -26,31 +34,26 @@ app.use('/api/auth', authRouter);
 app.use('/api', markerRouter);
 
 app.use(
-  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-    skip: (req, res) => process.env.NODE_ENV === 'test',
-  })
+    morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+        skip: (req, res) => process.env.NODE_ENV === 'test',
+    })
 );
 
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN,
-  })
-);
 
 function runServer(port = PORT) {
-  const server = app
-    .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
-    })
-    .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
-    });
+    const server = app
+        .listen(port, () => {
+            console.info(`App listening on port ${server.address().port}`);
+        })
+        .on('error', err => {
+            console.error('Express failed to start');
+            console.error(err);
+        });
 }
 
 if (require.main === module) {
-  dbConnect();
-  runServer();
+    dbConnect();
+    runServer();
 }
 
 module.exports = { app };
