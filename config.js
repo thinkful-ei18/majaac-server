25e32a510093b387d8519d7ee16e130c97ad3631
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-05-02 10:09:33 -0700

    mongoose test database

--- config.js
+++ config.js
@@ -6,8 +6,7 @@ module.exports = {
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/safer',
   TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL ||
-    'mongodb://localhost/thinkful-backend-test',
+    process.env.TEST_DATABASE_URL || 'mongodb://localhost/safer-test',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
 };
