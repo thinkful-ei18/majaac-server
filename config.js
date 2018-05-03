164ea1f5c0d71f0fee6b590a414230afd32c2319
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-05-03 12:25:32 -0700

    Mlab now working

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 'use strict';
 require('dotenv').config();
+const uri = require('./keys');
 
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/safer',
+  DATABASE_URL: process.env.DATABASE_URL || uri,
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL || 'mongodb://localhost/safer-test',
   JWT_SECRET: process.env.JWT_SECRET,
