593bbbde551fa3c8950b15c86496a0f41951adbf
alishaevn <alieva.inc@gmail.com>
2018-05-03 14:52:02 -0700

    added error handling

--- index.js
+++ index.js
@@ -43,6 +43,23 @@ app.use(
   })
 );
 
+
+/* ========== ERROR HANDLING ========== */
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {}
+  });
+});
+
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
