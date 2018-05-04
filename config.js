4f993c70adba0d9a5f9e4a81f92a817d3b9a8c0b
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-05-04 10:13:31 -0700

    env for mongodb uri

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 'use strict';
 require('dotenv').config();
-const uri = require('./keys');
 
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL: uri,
+  DATABASE_URL: process.env.MONGODB_URI,
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL || 'mongodb://localhost/safer-test',
   JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
